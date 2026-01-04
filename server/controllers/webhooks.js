310d25bbd8327a06a65c37297d50324be2abce19
r271205 <bhattirajan007@gmail.com>
2026-01-04 17:04:53 +0530

    Add error handling for missing webhook instance in clerkwebhooks

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,4 +1,4 @@
-/** 
+
 import { Webhook } from "svix";
 import User from"../models/User.js"
 
@@ -8,6 +8,9 @@ export const clerkwebhooks = async (req,res) => {
     try {
         // create a Svix instance with clerk webhook secret.
         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        if(!whook) {
+            return res.status(400).json({success:false,message:"Webhook not found"})
+        } 
 
         //verifying Headers
         await whook.verify(JSON.stringify(req.body),{
@@ -65,57 +68,57 @@ export const clerkwebhooks = async (req,res) => {
         res.json({success:false,message:'webhooks Error!'})
     }
 }  
-    */
+    
 
-import { Webhook } from "svix";
-import User from "../models/User.js";
+// import { Webhook } from "svix";
+// import User from "../models/User.js";
 
-export const clerkwebhooks = async (req, res) => {
-  try {
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+// export const clerkwebhooks = async (req, res) => {
+//   try {
+//     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    const payload = req.body.toString("utf8"); // raw body
-    const headers = {
-      "svix-id": req.headers["svix-id"],
-      "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req.headers["svix-signature"],
-    };
+//     const payload = req.body.toString("utf8"); // raw body
+//     const headers = {
+//       "svix-id": req.headers["svix-id"],
+//       "svix-timestamp": req.headers["svix-timestamp"],
+//       "svix-signature": req.headers["svix-signature"],
+//     };
 
-    const event = whook.verify(payload, headers);
-    const { data, type } = JSON.parse(payload);
+//     const event = whook.verify(payload, headers);
+//     const { data, type } = JSON.parse(payload);
 
-    switch (type) {
-      case "user.created": {
-        const userData = {
-          _id: data.id, // Clerk user id
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          image: data.image_url,
-          resume: "",
-        };
-        await User.create(userData);
-        break;
-      }
-      case "user.updated": {
-        const userData = {
-          email: data.email_addresses[0].email_address,
-          name: data.first_name + " " + data.last_name,
-          image: data.image_url,
-        };
-        await User.findByIdAndUpdate(data.id, userData);
-        break;
-      }
-      case "user.deleted": {
-        await User.findByIdAndDelete(data.id);
-        break;
-      }
-      default:
-        console.log("Unhandled event: ", type);
-    }
+//     switch (type) {
+//       case "user.created": {
+//         const userData = {
+//           _id: data.id, // Clerk user id
+//           email: data.email_addresses[0].email_address,
+//           name: data.first_name + " " + data.last_name,
+//           image: data.image_url,
+//           resume: "",
+//         };
+//         await User.create(userData);
+//         break;
+//       }
+//       case "user.updated": {
+//         const userData = {
+//           email: data.email_addresses[0].email_address,
+//           name: data.first_name + " " + data.last_name,
+//           image: data.image_url,
+//         };
+//         await User.findByIdAndUpdate(data.id, userData);
+//         break;
+//       }
+//       case "user.deleted": {
+//         await User.findByIdAndDelete(data.id);
+//         break;
+//       }
+//       default:
+//         console.log("Unhandled event: ", type);
+//     }
 
-    res.json({ success: true });
-  } catch (error) {
-    console.error("Webhook error:", error.message);
-    res.status(400).json({ success: false, message: "Webhook Error!" });
-  }
-};
+//     res.json({ success: true });
+//   } catch (error) {
+//     console.error("Webhook error:", error.message);
+//     res.status(400).json({ success: false, message: "Webhook Error!" });
+//   }
+// };
